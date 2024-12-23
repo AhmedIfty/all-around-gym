b5829459c1129666383dc78223bf1f4e1361c378
Iftekhar Ahmed <iftekhar.ahmed.ifty16@gmail.com>
2024-12-23 21:41:35 +0600

    added planType to db

--- bs.app/src/App.jsx
+++ bs.app/src/App.jsx
@@ -11,6 +11,9 @@ import SinglePage from './routes/singlePage/singlePage';
 import TrainerProfile from './routes/trainer_profile/trainer_profile';
 import Searchtest from './routes/searchtest/searchtest';
 import Payment from './routes/payment/payment';
+import SuccessPage from './routes/success/SuccessPage'; // Import the Success page
+// import CancelPage from './routes/cancel/CancelPage'; // Import the Cancel page
+
 import {
   createBrowserRouter,
   RouterProvider,
@@ -72,7 +75,15 @@ const App = () => {
     {
       path: "/Payment",
       element: <Payment/>
-    }
+    },
+    {
+      path: "/success", // Add route for success
+      element: <SuccessPage />,
+    },
+    // {
+    //   path: "/cancel", // Add route for cancel
+    //   element: <CancelPage />,
+    // },
     
   ]);
   
---
+++ bs.app/src/routes/cancel/CanclePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CancelPage = () => {
+    return (
+      <div>
+        <h1>Payment Successful!</h1>
+        <p>Your membership will be updated shortly. Thank you!</p>
+      </div>
+    );
+  };
+  
+  export default CancelPage;
