8e39a19c06bed59687832a9e9f2b9444b01c7802
AlveIbrahim <alnfibrahim@gmail.com>
2024-12-24 04:00:29 +0500

    search in gymlist, bmi to diet plan, diet plan scss

--- bs.app/src/App.jsx
+++ bs.app/src/App.jsx
@@ -63,7 +63,7 @@ const App = () => {
           element: <ProfileUpdatePage/>
         },
         {
-          path: "/TrainerProfile",
+          path: "/trainerprofile",
           element: <TrainerProfile/>
         },
         {
